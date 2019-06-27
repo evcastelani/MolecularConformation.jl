bbb73d60809cfaeee8b5134d0c1167e46c0ee49e
evcastelani <emersonvitor@gmail.com>
2019-06-27 16:01:00 -0300

    trying optimize again

--- src/solvers.jl
+++ src/solvers.jl
@@ -36,7 +36,11 @@ function quaternion_bp(n :: Int,
 		ρ = 1
 		cθ,sθ = qbondangle(i,D)
 		cω,sω = qtorsionangle(i,D)
-		Q[i] = Quaternion(sθ*cω,sθ*sω,-cθ*sω,cθ*cω)
+		a = sθ*cω
+		b = sθ*sω
+		c = -cθ*sω
+		d = cθ*cω
+		Q[i] = Quaternion(a,b,c,d)
 		Q[i] = Q[i-1]*Q[i]
 		qmol = Q[i]*Quaternion(0.0,D[i,i-1],0.0,0.0)*conj(Q[i])
 		mol.atoms[i].x = qmol.v1 + mol.atoms[i-1].x
@@ -64,8 +68,8 @@ function quaternion_bp(n :: Int,
 			@goto exit
 		end
 #		@info "partial solution in λ =$(λ) " sol
-		Q[i] = conj(Q[i])
-		#Q[i] = Quaternion(sθ*cω,-sθ*sω,cθ*sω,cθ*cω)
+		
+		Q[i] = Quaternion(a,-b,-c,d)
 		Q[i] = Q[i-1]*Q[i]
 		qmol = Q[i]*Quaternion(0.0,D[i,i-1],0.0,0.0)*conj(Q[i])
 		mol.atoms[i].x = qmol.v1 + mol.atoms[i-1].x
