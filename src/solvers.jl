0945122b4d4d1490d1468310b155c42642c07f13
evcastelani <emersonvitor@gmail.com>
2019-12-06 14:48:06 -0300

    fixed debug message

--- src/solvers.jl
+++ src/solvers.jl
@@ -96,7 +96,7 @@ function classicBP(NMRdata :: NMRType,
 			#@info "LDE = " LDE(mol,D,n,nad)
 			@goto exit
 		end
-		@debug "value of cθ,sθ ", cθ,sθ
+		@debug "value of c,s for all ", cθ,sθ
 		B = torsionmatrix(cθ,sθ,cω,sω,D34,'-')
 		C[l] = C[l-1]*B
 		mol.atoms[l].x = C[l][1,4]
