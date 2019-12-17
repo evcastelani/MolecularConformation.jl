28e79507f841ba60c2840f00910c0396296e7544
evcastelani <emersonvitor@gmail.com>
2019-12-17 13:47:03 -0300

    fixed option for all solutions

--- test/testmainfunction.jl
+++ test/testmainfunction.jl
@@ -1,7 +1,7 @@
 using DelimitedFiles
 @testset "Testing main functions" begin
 
-options = ConformationSetup(0.00001,classicBP,false)
+options = ConformationSetup(0.00001,classicBP,true)
 data = nmr("ins_15v2.pdb")
 
 sol = conformation(data,options)
