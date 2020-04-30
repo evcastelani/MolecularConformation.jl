e3426d4875aa644797da5e5e65619014b6e9cb80
evcastelani <emersonvitor@gmail.com>
2020-04-30 17:02:09 -0300

    testing changes in classicBP

--- src/solvers.jl
+++ src/solvers.jl
@@ -336,7 +336,7 @@ function classicBP(NMRdata :: NMRType,
 					nop_vpath += [5,3,0,0]
 
 				else
-					copy!(C_before,Virtual_Torsion) 
+					copyto!(C_before,Virtual_Torsion) 
 					nop_vpath += [24,33,0,0]
 				end
 				@debug "virtual atom position  " C_before[1,4],C_before[2,4],C_before[3,4]
